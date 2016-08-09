e3e381c56283f52a52877fa50b9d1159636f148d
Mahesh Babariya <mahi.mb1991@gmail.com>
2016-08-09 17:52:33 +0530

    Update AppMethod.java

--- app/src/main/java/com/mahii/Global/AppMethod.java
+++ app/src/main/java/com/mahii/Global/AppMethod.java
@@ -868,5 +868,47 @@ public class AppMethod {
     }
     /* To use :- Log.e("MaHi", RandomString.getString(5)); */
     /* END */
+    
+    /*START Save hash map to file */
+	public static void saveHashMapToFile(Context context, String fileName, HashMap<String, String> h) {
+		String filePath = context.getFilesDir().getPath() + "/" + fileName;
+		File f = new File(filePath);
+		try {
+			if (!f.exists()) {
+				f.createNewFile();
+			}
+
+			ObjectOutputStream objOSt = new ObjectOutputStream(new FileOutputStream(f));
+			objOSt.writeObject(h);
+			objOSt.flush();
+			objOSt.close();
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/* END */
+
+	/*START Load hash map from file hash map */
+	public static HashMap<String, String> loadHashMapFromFile(Context context, String fileName) {
+		String filePath = context.getFilesDir().getPath() + "/" + fileName;
+		File f = new File(filePath);
+		try {
+			if (f.exists()) {
+
+				ObjectInputStream objISt = new ObjectInputStream(new FileInputStream(f));
+
+				HashMap<String, String> h = (HashMap<String, String>) objISt.readObject();
+				objISt.close();
+				System.out.println(h.get("c"));
+				return h;
+			} else {
+				System.out.println("FILE DOESNOT EXIST !!");
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return new HashMap<>(1);
+	}
 
 }
