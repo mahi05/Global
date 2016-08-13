2da30bc19c77e575973dc2b85670b20633c47f5b
Mahesh Babariya <mahi.mb1991@gmail.com>
2016-08-13 12:35:03 +0530

    Update AppMethod.java

--- app/src/main/java/com/mahii/Global/AppMethod.java
+++ app/src/main/java/com/mahii/Global/AppMethod.java
@@ -949,5 +949,21 @@ public class AppMethod {
 		return jsonObject;
 	}
 	/* END */
+	
+	/* START Check if Service is running or not */
+	public static boolean isServiceRunning(Context context, Class<?> serviceClass) {
+		try {
+			ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+			for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+				if (serviceClass.getName().equals(service.service.getClassName())) {
+					return true;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	/* END */
 
 }
