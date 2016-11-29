3693454b6743ef813675fb99e35c3440e7f78b77
Mahesh Babariya <mahi.mb1991@gmail.com>
2016-11-29 14:18:56 +0530

    Static method: Disable soft keyboard

--- app/src/main/java/com/mahii/Global/AppMethod.java
+++ app/src/main/java/com/mahii/Global/AppMethod.java
@@ -1064,5 +1064,20 @@ public class AppMethod {
 		return (freeSpace);
 	}
 	/* END */
+	
+	/** 
+	 * Disable soft keyboard from appearing, use in conjunction with android:windowSoftInputMode="stateAlwaysHidden|adjustNothing" 
+	 * @param editText 
+	 */ 
+	public static void disableSoftInputFromAppearing(EditText editText) {
+		if (Build.VERSION.SDK_INT >= 11) {
+			editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+			editText.setTextIsSelectable(true);
+		} else { 
+			editText.setRawInputType(InputType.TYPE_NULL);
+			editText.setFocusable(true);
+		} 
+	}
+	/* END */
 
 }
